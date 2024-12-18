49cd43fb66002ad7cefefa77d0f1af6c3751a840
tty-hashi <hashi.ndig@gmail.com>
2024-12-18 15:35:59 +0900

    prettier と linter の設定を追加

--- aws-deploy-test/eslint.config.mjs
+++ aws-deploy-test/eslint.config.mjs
@@ -11,6 +11,35 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-console": "error",
+
+      "prefer-const": "off",
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-expect-error": "allow-with-description"
+        }
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^[e_]",
+          "varsIgnorePattern": "^_",
+          "caughtErrors": "none"
+        }
+      ],
+      "no-unused-expressions": ["error", { "allowTernary": true }],
+      "linebreak-style": ["error", "unix"],
+    },
+  },
 ];
 
 export default eslintConfig;
